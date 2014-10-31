a34212fc9959b70ba512582ce55548446f7421eb
Rémi Andrieux <pimolo10300@gmail.com>
2014-10-31 14:15:36 +0100

    Affichages du menu et des articles simplifiés

--- src/Novify/FrontBundle/Controller/FrontController.php
+++ src/Novify/FrontBundle/Controller/FrontController.php
@@ -16,19 +16,6 @@ class FrontController extends Controller
         return $this->render('NovifyFrontBundle:Front:menu.html.twig', array('categories' => $categories));
     }
 
-    // controller appelé dans menu.html.twig pour affichier les sous catégories
-    public function showMenuSousCategoriesAction($categorie)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $cat = $em->getRepository('NovifyModelBundle:Categories')->findOneBycatNom($categorie);
-        $souscats = $em->getRepository('NovifyModelBundle:Souscategories')->findByCategorie($cat);
-        if (!$cat) {
-            throw new NotFoundHttpException("Cette catégorie n'existe pas.");
-        }
-        // récupère les sous catégories pour le controller qui les affiche dans le menu via un for
-        return $this->render('NovifyFrontBundle:Front:menu_souscategories.html.twig', array('souscats' => $souscats, 'categorie' => $cat));
-    }
-
     public function indexAction()
     {
         return $this->render('NovifyFrontBundle:Front:index.html.twig');
@@ -77,19 +64,6 @@ class FrontController extends Controller
         }
 
         return $this->render('NovifyFrontBundle:Front:catalogue.html.twig', array('categorie' => $cat, 'souscats' => $souscat));
-    }
-
-    public function showArticlesAction($categorie, $sousCategorie)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $cat = $em->getRepository('NovifyModelBundle:Categories')->findOneBycatNom($categorie);
-        $souscat = $em->getRepository('NovifyModelBundle:Souscategories')->findOneBy(array('categorie' => $cat, 'souscatNom' => $sousCategorie));
-        $articles = $em->getRepository('NovifyModelBundle:Articles')->findBysousCategorie($souscat);
-        if (!$souscat) {
-            throw new NotFoundHttpException("Cette sous-catégorie n'existe pas.");
-        }
-
-        return $this->render('NovifyFrontBundle:Front:catalogue_articles.html.twig', array('categorie' => $cat, 'souscat' => $souscat, 'articles' => $articles));
     }
 
     public function viewoneAction($categorie, $sousCategorie, $id)
