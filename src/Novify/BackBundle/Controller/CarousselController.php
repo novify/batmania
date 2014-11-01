d807053b77f7378e2341facd053c52293a4d1485
Maxime Emorine <maxime.emorine@hotmail.fr>
2014-11-01 11:06:04 +0100

    Gestion du carousel dans le backoff

---
+++ src/Novify/BackBundle/Controller/CarousselController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Novify\BackBundle\Controller;
+
+use Novify\ModelBundle\Form\CarousselType;
+use Novify\ModelBundle\Entity\Caroussel;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CarousselController extends Controller
+{
+    public function helloAction($name)
+    {
+        return $this->render('NovifyBackBundle:Caroussel:hello.html.twig', array('name' => $name));
+    }
+
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $caroussels = $em->getRepository('NovifyModelBundle:Caroussel')->findAll();
+
+        return $this->render('NovifyBackBundle:Caroussel:index.html.twig', array('caroussels' => $caroussels));
+    }
+
+    public function addAction(Request $request)
+    {
+        $caroussel = new Caroussel();
+        $form = $this->createForm(new CarousselType(), $caroussel);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($caroussel);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('novify_back_caroussel_index'));
+        }
+
+        return $this->render('NovifyBackBundle:Caroussel:add.html.twig', array('form' => $form->createView()));
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $caroussel = $em->getRepository('NovifyModelBundle:Caroussel')->find($id);
+        if (!$caroussel) {
+            throw new NotFoundHttpException("Cette slide du carrousel n'existe pas");
+        }
+        $form = $this->createForm(new CarousselType(), $caroussel);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em->persist($caroussel);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('novify_back_caroussel_index'));
+        }
+
+        return $this->render('NovifyBackBundle:Caroussel:edit.html.twig', array('form' => $form->createView()));
+    }
+
+    public function removeAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $caroussel = $em->getRepository('NovifyModelBundle:Caroussel')->find($id);
+        $em->remove($caroussel);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('novify_back_caroussel_index'));
+    }
+}
